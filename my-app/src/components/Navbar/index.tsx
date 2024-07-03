e157d46932693b11410a6f8d87508292feba0a22
isnendyankp <isnendyankp@gmail.com>
2024-07-03 19:47:55 +0700

    chore: Update Navbar component to include logo link

--- my-app/src/components/Navbar/index.tsx
+++ my-app/src/components/Navbar/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <div className='bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0'>
-      <div>Login</div>
+      <Link href='/'>Logo</Link>
     </div>
   );
 }
