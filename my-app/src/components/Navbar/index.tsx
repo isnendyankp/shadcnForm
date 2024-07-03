8d3c3d32638758f4d75ac050677853ab2cdc6b9a
isnendyankp <isnendyankp@gmail.com>
2024-07-03 19:55:22 +0700

    chore: Update Navbar component to include HandMetal icon in logo link

--- my-app/src/components/Navbar/index.tsx
+++ my-app/src/components/Navbar/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
 import { buttonVariants } from '../ui/button';
+import { HandMetal } from 'lucide-react';
 
 const Navbar = () => {
   return (
     <div className="bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
       <div className='container flex items-center justify-between'>
-        <Link href="/">Logo</Link>
+        <Link href="/"><HandMetal/></Link>
         <Link className={buttonVariants()} href="/sign-in">
           Sign In
         </Link>
