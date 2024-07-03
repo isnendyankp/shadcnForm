afda70f2d8a69e6d5eee806ba19e9a506b902b9e
isnendyankp <isnendyankp@gmail.com>
2024-07-03 19:50:44 +0700

    chore: Refactor Navbar component to include logo link and Sign In button

--- my-app/src/components/Navbar/index.tsx
+++ my-app/src/components/Navbar/index.tsx
@@ -4,9 +4,13 @@ import { buttonVariants } from '../ui/button';
 
 const Navbar = () => {
   return (
-    <div className='bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0'>
-      <Link href='/'>Logo</Link>
-      <Link className={buttonVariants()} href='/sign-in'>Sign In</Link>
+    <div className="bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
+      <div className='container flex items-center justify-between'>
+        <Link href="/">Logo</Link>
+        <Link className={buttonVariants()} href="/sign-in">
+          Sign In
+        </Link>
+      </div>
     </div>
   );
 }
