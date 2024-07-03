07d91147b8feb565b3f2307b0e0ddc87abbe04f3
isnendyankp <isnendyankp@gmail.com>
2024-07-03 19:40:48 +0700

    chore: Update Navbar component styling and add login button

--- my-app/src/components/Navbar/index.tsx
+++ my-app/src/components/Navbar/index.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <div className='bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0'>
+      <div>Login</div>
+    </div>
+  );
 }
 
 export default Navbar
